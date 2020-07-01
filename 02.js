660c476abfd4b497b1d5f6c87190cf769ff487f4
Yosi Azwan <yosi.azwan@gmail.com>
2020-07-01 19:12:59 +0700

    soal 2

--- 02.js
+++ 02.js
@@ -1,3 +1,7 @@
-const first = ['Behind', 'every', 'great', 'man']
-const second = ['is', 'a', 'woman']
-const third = ['rolling', 'her', 'eyes']
+const first = ['Behind', 'every', 'great', 'man'];
+const second = ['is', 'a', 'woman'];
+const third = ['rolling', 'her', 'eyes'];
+
+const arrayJoin = first.join(" ") + " " + second.join(" ") + " " + third.join(" ");
+
+console.log(arrayJoin);
